6fde3c05fcd5219c4bd2c0e7e87ebbbb421e56ec
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-08-08 17:15:15 -0500

    Agregando firebase y formulario

---
+++ src/js/firebase.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyADOXIX0vc5htFOg4MtSEdlKOl-gbj03vk",
+    authDomain: "registro-de-usuarios-3bd72.firebaseapp.com",
+    databaseURL: "https://registro-de-usuarios-3bd72.firebaseio.com",
+    projectId: "registro-de-usuarios-3bd72",
+    storageBucket: "registro-de-usuarios-3bd72.appspot.com",
+    messagingSenderId: "469429918272"
+  };
+  firebase.initializeApp(config);
