82cc130a78af5da993bfbd087e19de26d6a23c3c
Citlalli DMG <citlallidmg@gmail.com>
2018-08-09 10:25:27 -0500

    agrega estilos y cambia flujo de la aplicacion

--- src/js/register.js
+++ src/js/register.js
@@ -1,41 +1,8 @@
-// Get a reference to the database service
-var database = firebase.database();
-console.log(database);
+const returnButton = document.getElementById('return-button');
 
-// Constant to save URL of companies json file
-const json = 'https://raw.githubusercontent.com/Anavzqz/Registro_Visitantes/rama-Ana/data/data.json';
+returnButton.addEventListener('click', () =>{
+  console.log('holi');
 
-// Function to get companies json
-const getData = () => {
-  console.log('hola');
-  fetch(json)
-    .then(response => response.json())
-    .then((res) => {
-      console.log(res);
-    });
-  getInputs();
-};
+  window.location.assign('../index.html');
+});
 
-const getInputs = () => {
-  // Get user inputs
-  const inputName = document.getElementById('input-name');
-  const inputEmail = document.getElementById('input-email');
-  const inputCompany = document.getElementById('input-company');
-  const inputHost = document.getElementById('input-visited');
-  const buttonLogIn = document.getElementById('log-in');
-
-  initRegister(inputName.value, inputEmail.value, inputCompany.value, inputHost.value);
-};
-
-
-const getDataUser = () => {
-  console.log('hola1');
-};
-
-const validateDataUser = () => {
-  console.log('hola');
-};
-
-
-
-window.onload = getData;
