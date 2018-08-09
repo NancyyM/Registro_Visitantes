f66b6ccd03368343d932a275f1df8e1e2658d36e
Citlalli DMG <citlallidmg@gmail.com>
2018-08-09 16:09:07 -0500

    baja cambios de master

--- src/js/player.js
+++ src/js/player.js
@@ -1,27 +1,27 @@
-let player = document.getElementById('player'); 
-  let snapshotCanvas = document.getElementById('snapshot');
-  let captureButton = document.getElementById('capture');
-  let videoTracks;
+let player = document.getElementById('player');
+let snapshotCanvas = document.getElementById('snapshot');
+let captureButton = document.getElementById('capture');
+let videoTracks;
 
-  let handleSuccess = function(stream) {
-    // Attach the video stream to the video element and autoplay.
-    player.srcObject = stream;
-    videoTracks = stream.getVideoTracks();
-  };
+let handleSuccess = function (stream) {
+  // Attach the video stream to the video element and autoplay.
+  player.srcObject = stream;
+  videoTracks = stream.getVideoTracks();
+};
 
-  captureButton.addEventListener('click', function() {
-    let context = snapshot.getContext('2d');
-    // Draw the video frame to the canvas.
-    context.drawImage(player, 0, 0, snapshotCanvas.width, 
-        snapshotCanvas.height);
+captureButton.addEventListener('click', function () {
+  let context = snapshot.getContext('2d');
+  // Draw the video frame to the canvas.
+  context.drawImage(player, 0, 0, snapshotCanvas.width,
+    snapshotCanvas.height);
 
-        // Stop all video streams.
-    videoTracks.forEach(function(track) {track.stop()});
-  });
+  // Stop all video streams.
+  videoTracks.forEach(function (track) { track.stop() });
+});
 
-  navigator.mediaDevices.getUserMedia({video: true})
-      .then(handleSuccess);
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(handleSuccess);
 
-  let imageDataURL = hidden_canvas.toDataURL('image/png');
-      // Set the dataURL as source of an image element, showing the captured photo.
-      image.setAttribute('src', imageDataURL); 
+let imageDataURL = hidden_canvas.toDataURL('image/png');
+// Set the dataURL as source of an image element, showing the captured photo.
+image.setAttribute('src', imageDataURL);
