259d4dc8c7cb4f371a49af6243d1c296cd766931
Citlalli DMG <citlallidmg@gmail.com>
2018-08-09 11:17:21 -0500

    agrega funcionalidad que escucha los inputs, activa buton para siguiente vista y guarda datos en objeto

--- src/js/register.js
+++ src/js/register.js
@@ -1,8 +1,42 @@
+// Get inputs from register page
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+
+// Get buttons from register page
 const returnButton = document.getElementById('return-button');
+const nextButton = document.getElementById('next-button');
 
-returnButton.addEventListener('click', () =>{
-  console.log('holi');
+// Create objet for visitor data
+const visitorData = {};
 
+// Add listener to buttons
+returnButton.addEventListener('click', () => {
   window.location.assign('../index.html');
 });
 
+nextButton.addEventListener('click', () => {
+  console.log('holi');
+});
+
+// Listen to visitor name
+inputName.addEventListener('keyup', () => {
+  visitorName = inputName.value.trim();
+  if (visitorName != '') {
+    inputLastName.removeAttribute('disabled');
+    visitorData.name = visitorName;
+    // console.log(visitorData);
+    listenInputLastName(visitorData);
+  };
+});
+
+// Listen to visitor last name
+const listenInputLastName = (visitorData) => {
+  inputLastName.addEventListener('keyup', () => {
+    visitorLastName = inputLastName.value.trim();
+    if (visitorLastName != '') {
+      visitorData.lastname = visitorLastName;
+      nextButton.classList.remove('disabled');
+      console.log(visitorData);
+    };
+  });
+};
