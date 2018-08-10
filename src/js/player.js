a547c0e5da5e033da9424fe15fcf617cf65c70dc
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-08-09 19:29:14 -0500

    Cambios player

--- src/js/player.js
+++ src/js/player.js
@@ -1,35 +1,40 @@
-let player = document.getElementById('player'); 
+  let player = document.getElementById('player'); 
   let snapshotCanvas = document.getElementById('snapshot');
   let captureButton = document.getElementById('capture');
   let videoTracks;
 
-  let handleSuccess = function(stream) {
+  let handleSuccess = (stream) => {
     // Attach the video stream to the video element and autoplay.
     player.srcObject = stream;
     videoTracks = stream.getVideoTracks();
   };
 
-  captureButton.addEventListener('click', function() {
-    let context = snapshot.getContext('2d');
+  captureButton.addEventListener('click', () => {
+  let context = snapshot.getContext('2d');
     // Draw the video frame to the canvas.
     context.drawImage(player, 0, 0, snapshotCanvas.width, 
         snapshotCanvas.height);
 
-        let imageDataURL = snapshotCanvas.toDataURL();
+  let imageDataURL = snapshotCanvas.toDataURL();
 
-// Create a root reference
-let storageRef = firebase.storage().ref();
-let images = storageRef.child('Images');
+  // Create a root reference
+  let storageRef = firebase.storage().ref();
+  let images = storageRef.child('Images');
 
-images.putString(imageDataURL, 'data_url').then(function(snapshot) {
+  images.putString(imageDataURL, 'data_url').then(function(snapshot) {
   console.log('Uploaded a data_url string!');
-});
+  });
 
         // Stop all video streams.
     videoTracks.forEach(function(track) {track.stop()});
   });
 
-//Detiene la camara cuando se toma la foto
+  //Detiene la camara cuando se toma la foto
   navigator.mediaDevices.getUserMedia({video: true})
       .then(handleSuccess);
 
+let next = document.getElementById('next-button-player');
+
+next.addEventListener('click', () => {
+  window.location.assign('../src/');
+});
